87108a057d3350c1fde05a6ce07d7e0234d9dac6
Ramy Tanios <ramy.tanios@gmail.com>
2026-01-19 10:47:14 +0100

    wip

--- src/client.py
+++ src/client.py
@@ -1,5 +1,6 @@
 import asyncio
 from asyncio import Queue
+from asyncio.queues import QueueFull
 from dataclasses import dataclass, replace
 from typing import Callable, Optional
 
@@ -10,6 +11,7 @@ from textual.app import App, ComposeResult
 from textual.reactive import reactive
 from textual.widget import Widget
 from textual.widgets import Footer, Header
+from textual.widgets._select import Select
 from textual.widgets._static import Static
 from textual_plotext import PlotextPlot
 
@@ -18,6 +20,7 @@ from message import (
     Conventions,
     FullArbitrageCheck,
     Ping,
+    Pong,
     Rates,
     ServerMsg,
     VolSamples,
@@ -68,30 +71,79 @@ async def ws_async(q_in: Queue[ServerMsg], q_out: Queue[ClientMsg]) -> None:
             log.error(f"WS connection failed in task group: ${e.exceptions}")
 
 
+@dataclass
+class State:
+    rates: Optional[Rates] = None
+    conventions: Optional[Conventions] = None
+    matrix: Optional[FullArbitrageCheck] = None
+    samples: Optional[VolSamples] = None
+
+    def modify(
+        self,
+        rates: Optional[Rates] = None,
+        conventions: Optional[Conventions] = None,
+        matrix: Optional[FullArbitrageCheck] = None,
+        samples: Optional[VolSamples] = None,
+    ):
+        return replace(
+            self,
+            rates=rates,
+            conventions=conventions,
+            matrix=matrix,
+            samples=samples,
+        )
+
+
 class RatesConventions(Widget):
     DEFAULT_CLASSES = "box"
     BORDER_TITLE = "Rates & Conventions"
 
+    rates: reactive[Optional[Rates]] = reactive(None)
+
+    conventions: reactive[Optional[Conventions]] = reactive(None)
+
     def compose(self) -> ComposeResult:
-        yield Static("TODO")
+        if self.rates:
+            yield Select(
+                list(self.rates.libor_rates.items()),
+                value=0,
+                allow_blank=False,
+                compact=True,
+            )
+            yield Select(
+                list(self.rates.swap_rates.items()),
+                value=0,
+                allow_blank=False,
+                compact=True,
+            )
 
 
 class VolaSkewChart(Widget):
     DEFAULT_CLASSES = "box"
     BORDER_TITLE = "Volatility Smile"
 
+    samples: reactive[Optional[VolSamples]] = reactive(None)
+
     def compose(self) -> ComposeResult:
         yield PlotextPlot()
 
-    def on_mount(self) -> None:
-        plt = self.query_one(PlotextPlot).plt
-        y = plt.sin()
-        plt.scatter(y)
+    def watch_samples(self, samples: Optional[VolSamples]) -> None:
+        if samples:
+            plt = self.query_one(PlotextPlot).plt
+            plt.cld()
+            ks = samples.samples.strikes
+            pdf = samples.samples.pdf
+            qks = samples.samples.quoted_strikes
+            qpdf = samples.samples.quoted_vols
+            plt.plot(ks, pdf, color="yellow")
+            plt.scatter(qks, qpdf, marker="o", color="orange")
 
 
 class ArbitrageMatrix(Widget):
     DEFAULT_CLASSES = "box"
     BORDER_TITLE = "Arbitrage Matrix"
+
+    matrix: reactive[Optional[FullArbitrageCheck]] = reactive(None)
 
     def compose(self) -> ComposeResult:
         yield Static("TODO")
@@ -101,41 +153,40 @@ class DensityChart(Widget):
     DEFAULT_CLASSES = "box"
     BORDER_TITLE = "Implied Probability Density"
 
+    samples: reactive[Optional[VolSamples]] = reactive(None)
+
     def compose(self) -> ComposeResult:
         yield PlotextPlot()
 
-    def on_mount(self) -> None:
-        plt = self.query_one(PlotextPlot).plt
-        y = plt.sin()
-        plt.scatter(y)
+    def watch_samples(self, samples: Optional[VolSamples]) -> None:
+        if samples:
+            plt = self.query_one(PlotextPlot).plt
+            plt.cld()
+            ks = samples.samples.strikes
+            pdf = samples.samples.pdf
+            qks = samples.samples.quoted_strikes
+            qpdf = samples.samples.quoted_pdf
+            plt.plot(ks, pdf, color="yellow")
+            plt.scatter(qks, qpdf, marker="o", color="orange")
 
 
 class Body(Widget):
+    state: reactive[Optional[State]] = reactive(None)
+
     def compose(self) -> ComposeResult:
         yield RatesConventions()
         yield VolaSkewChart()
         yield ArbitrageMatrix()
         yield DensityChart()
 
-
-@dataclass
-class State:
-    rates: Optional[Rates] = None
-    conventions: Optional[Conventions] = None
-    matrix: Optional[FullArbitrageCheck] = None
-    samples: Optional[VolSamples] = None
-
-    def with_rates(self, v: Rates):
-        return replace(self, rates=v)
-
-    def with_conventions(self, v: Rates):
-        return replace(self, conventions=v)
-
-    def with_matrix(self, v: FullArbitrageCheck):
-        return replace(self, matrix=v)
-
-    def with_samples(self, v: VolSamples):
-        return replace(self, samples=v)
+    def watch_state(self, state: State) -> None:
+        if not state:
+            return
+        self.query_one(RatesConventions).rates = state.rates
+        self.query_one(RatesConventions).conventions = state.conventions
+        self.query_one(VolaSkewChart).samples = state.samples
+        self.query_one(ArbitrageMatrix).matrix = state.matrix
+        self.query_one(DensityChart).samples = state.samples
 
 
 class Arbitui(App):
@@ -155,15 +206,54 @@ class Arbitui(App):
 
     q_in = Queue[ServerMsg]()
     q_out = Queue[ClientMsg]()
+    q_state_updates = Queue[Callable[[State], State]]()
 
     state: reactive[State] = reactive(State())
 
-    def update_state(self, f: Callable[[State], State]):
-        self.state = f(self.state)
+    def update_state(self, fn: Callable[[State], State]):
+        try:
+            self.q_state_updates.put_nowait(fn)
+        except QueueFull as e:
+            log.error(f"state update Q is full: {e}")
 
     async def on_mount(self) -> None:
         self.theme = self.THEME_DARK
         self.run_worker(ws_async(self.q_in, self.q_out), exit_on_error=False)
+        self.run_worker(self.recv_loop(), exit_on_error=False)
+        self.run_worker(self.state_updates_loop(), exit_on_error=False)
+
+    async def recv_loop(self):
+        while True:
+            try:
+                msg = await self.q_in.get()
+                self.handle_server_msg(msg)
+            except Exception:
+                log.error("exception in receive loop: {e}")
+
+    def handle_server_msg(self, msg: ServerMsg):
+        match msg:
+            case Pong():
+                log.info("pong received")
+            case Rates() as rates:
+                log.info("updating rates state")
+                self.update_state(lambda s: s.modify(rates=rates))
+            case Conventions() as conventions:
+                log.info("updating conventions state")
+                self.update_state(lambda s: s.modify(conventions=conventions))
+            case FullArbitrageCheck() as matrix:
+                log.info("updating arbitrage matrix state")
+                self.update_state(lambda s: s.modify(matrix=matrix))
+            case VolSamples() as samples:
+                log.info("updating vol samples state")
+                self.update_state(lambda s: s.modify(samples=samples))
+
+    async def state_updates_loop(self):
+        while True:
+            try:
+                fn = await self.q_state_updates.get()
+                self.state = fn(self.state)
+            except Exception as e:
+                log.error(f"state update failed: {e}")
 
     def action_toggle_dark(self) -> None:
         self.theme = (
@@ -172,7 +262,7 @@ class Arbitui(App):
 
     def compose(self) -> ComposeResult:
         yield Header(show_clock=True)
-        yield Body()
+        yield Body().data_bind(Arbitui.state)
         yield Footer()
 
 

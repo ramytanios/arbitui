b6d9e5dab44dc6808a108a301dff4fe5c5b7b6a7
Ramy Tanios <ramy.tanios@gmail.com>
2026-02-08 01:09:24 +0100

    cleanup settings

--- src/settings.py
+++ src/settings.py
@@ -6,7 +6,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     home: Path = Path.home() / ".local" / "share" / "arbitui"
     server_ws_url: str = "ws://localhost:8000/ws"
-    rpc_url: str = "http://localhost:8090/rpc" # TODO remove
     lib_socket_path: str = "/tmp/rates-scope.sock"
     file_search_path: str = "."
     max_requests_in_flight: int = 512
